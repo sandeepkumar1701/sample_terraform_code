f757dc135aa77a400890ef51398aea4497c76b31
Kunchala Vikram <kunchalavikram1427@gmail.com>
2023-10-31 08:24:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,12 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
+  count         = var.count
   ami           = var.ami
   instance_type = var.instance_type
 
   tags = {
-    Name        = "webserver"
-    Environment = "prod"
+    Name        = "webserver-${count.index}"
+    Environment = "dev"
   }
 }
